165f46040bc64ea61f4cbced2d105d399a84db67
linuxlite <eze.rey92@gmail.com>
2022-06-09 15:22:33 -0300

    vistas config nuevo concepto

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-const port = 3000;
 const express = require("express");
+const { engine } = require("express-handlebars");
 const app = express();
 const dataBase = require("./database");
 const routes = require("./routes/index.routes");
 
-const server = async () => {
-  await app.listen(port);
-  console.log(`server listening on port: ${port}`);
-};
+app.set("port", process.env.PORT || 3000);
 
+app.set("views", `${__dirname}/views`);
+
+app.engine(
+  ".hbs",
+  engine({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: `${app.get("views")}/layouts`,
+    partialsDir: `${app.get("views")}/partials`,
+  })
+);
+//server
+const server = async () => {
+  await app.listen(app.get("port"));
+  console.log(`server listening on port: ${app.get("port")}`);
+};
 server();
 app.use(routes);
