9423072f1da56a7afe5e2d536f9fccf7c039e2e0
linuxlite <eze.rey92@gmail.com>
2022-06-09 12:40:47 -0300

    agregando elementos como carpetas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const port = 3000;
 const express = require("express");
 const app = express();
 const dataBase = require("./database");
-const routes = require("../routes/index.routes");
+const routes = require("./routes/index.routes");
 
 const server = async () => {
   await app.listen(port);
