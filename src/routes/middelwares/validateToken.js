96abe0b5d60ce95ddcfbd822aec564301363455e
linuxlite <eze.rey92@gmail.com>
2022-06-12 22:50:44 -0300

    tokens terminado

--- src/routes/middelwares/validateToken.js
+++ src/routes/middelwares/validateToken.js
@@ -4,16 +4,18 @@ import TOKEN_SECRET from "../../config/config.js";
 export const verifyToken = (req, res, next) => {
   const token = req.header("token");
   if (!token)
-    res.status(401).json({ error: "Incluya el token en la cabeceras" });
+    res.status(401).json({
+      error: "Incluya el token en la cabeceras| ** token: 'su token' **",
+    });
   try {
     const verified = jwt.verify(token, TOKEN_SECRET.TOKEN_SECRET);
     const admin = verified.role.includes("ADMIN");
     if (admin) {
       next();
     } else {
-      res
-        .status(401)
-        .json({ error: `Ingrese el token en la cabeceras token:"su token"` });
+      res.status(401).json({
+        error: `Tiene que ser ADMIN para poder disponer de esta ruta`,
+      });
     }
   } catch (error) {
     console.log(
