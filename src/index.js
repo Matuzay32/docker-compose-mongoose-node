08276b06f67b38e8036b84245cb1d87da930fa84
linuxlite <eze.rey92@gmail.com>
2022-06-27 18:09:30 -0300

    antes de agreagar

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ import main from "./database.js";
 //File name system
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+console.log(__dirname, "__dirname");
+console.log(__filename, "filename");
 const app = express();
 app.use(cors());
 
